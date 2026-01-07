751c7a976239e29d81f322d34313a5bb6109ac4f
Teo Djian <teodjian@gmail.com>
2026-01-08 01:49:39 +0200

    feat: split the func for tests

--- car_inventory_service/src/initialize.py
+++ car_inventory_service/src/initialize.py
@@ -4,8 +4,13 @@ from car_inventory_service.src.api.endpoints import router
 from car_inventory_service.src.singleton.impls.configuration import Configuration
 
 
-def init_service():
-    config = Configuration().settings
+def create_app() -> FastAPI:
     app = FastAPI()
     app.include_router(router=router)
+    return app
+
+
+def init_service():
+    config = Configuration().settings
+    app = create_app()
     uvicorn.run(app, host=config.server_api.host, port=config.server_api.port)
