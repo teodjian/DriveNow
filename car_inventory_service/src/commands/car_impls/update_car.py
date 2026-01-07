7a5b0ba9e086b77463f67475f73181af1e8e5b5d
Teo Djian <teodjian@gmail.com>
2026-01-07 23:51:06 +0200

    fix: allowed none value in the update endpoint

--- car_inventory_service/src/commands/car_impls/update_car.py
+++ car_inventory_service/src/commands/car_impls/update_car.py
@@ -1,12 +1,12 @@
 from logging import Logger
 
 from car_inventory_service.src.commands.command_interface import ICommand
-from car_inventory_service.src.models.car import Car
+from car_inventory_service.src.models.car import CarToUpdate
 from car_inventory_service.src.repositories.car.car_repository import ICarRepository
 
 
 class UpdateCar(ICommand):
-    def __init__(self, repository: ICarRepository, logger: Logger, car: Car):
+    def __init__(self, repository: ICarRepository, logger: Logger, car: CarToUpdate):
         self._repository = repository
         self._logger = logger
         self._car = car
