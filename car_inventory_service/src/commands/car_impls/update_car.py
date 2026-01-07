03692d27c59127962e392856c9815957bee8f718
Teo Djian <teodjian@gmail.com>
2026-01-07 14:06:05 +0200

    feat: add command pattern implementation

---
+++ car_inventory_service/src/commands/car_impls/update_car.py
@@ -0,0 +1,15 @@
+from logging import Logger
+
+from car_inventory_service.src.commands.command_interface import ICommand
+from car_inventory_service.src.models.car import Car
+from car_inventory_service.src.repositories.car.car_repository import ICarRepository
+
+
+class UpdateCar(ICommand):
+    def __init__(self, repository: ICarRepository, logger: Logger, car: Car):
+        self._repository = repository
+        self._logger = logger
+        self._car = car
+
+    def execute(self):
+        self._repository.update(str(self._car.id), self._car.status)
